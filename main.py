ce3f306d109c8c7daa6164a9848ee56b0833914e
Jorge Lewis <antopiahk@gmail.com>
2023-08-02 11:57:21 +0800

    weekly log command

--- main.py
+++ main.py
@@ -1,57 +1,61 @@
+import asyncio
 import discord
 import time
+from discord.ext import commands
+
+from utils import calculate_duration, minutes_to_hours
 
 # Discord Stuff
 intents = discord.Intents.all()
 intents.message_content = True
 
-bot = discord.Client(intents=intents)
-
+client = discord.Client(intents=intents)
+client = commands.Bot(command_prefix='$', intents=intents)
 # Token Handling
 TOKEN: str = ''
-with open('efficiency-bot/Efficiency-Bot/not_public.txt', 'r') as tokenFile:
+with open('efficiency-bot/Efficiency-bot/not_public.txt', 'r') as tokenFile:
     TOKEN = tokenFile.read()
 
-logging_channel = bot.get_channel(1136091862839595018)
 
-# Logging & Calculating
-users_sessions_store = {'horheyjorge': 0, 'eksno': 0, 'herecomessebastianvettel': 0}
+# Logging & Calculating {'member.name': session_start_time}
+users_sessions = {'horheyjorge': 0, 'eksno': 0, 'herecomessebastianvettel': 0}
+
+logging_channel = None
 
 def member_joined_office(member):
-    start_clock(member)
-    text_to_write = member.display_name + " Joined Office @ " + str(time.time()) + "\n"
-    print(text_to_write)
-def member_left_office(member):
-    text_to_write = member.display_name + " Logged Out @ " + str(calculate_duration(member)) + "\n"
-    logging_channel.send(text_to_write)
-    with open("efficiency-bot/Efficiency-Bot/time_log.txt", "a") as time_log:
-        time_log.write(text_to_write)
+    start_clock_for_member(member)
 
-def start_clock(member):
-    users_sessions_store[member.name] = time.time()
+async def member_left_office(member):
+    session_duration = calculate_duration(member, users_sessions[member.name])
+    display_text = member.display_name + " Logged Out with a Duration of " + str(session_duration) + " Minutes"
+    logging_channel = client.get_channel(1136091862839595018)
+    await logging_channel.send(display_text)
+    log = member.display_name + " " +  str(session_duration) +  "\n"
+    with open("efficiency-bot/Efficiency-bot/time_logs.txt", "a") as time_logs:
+        time_logs.write(log)
 
-def calculate_duration(member):
-    start_time = users_sessions_store[member.name]
-    duration = time.time() - start_time
-    return seconds_to_minutes(duration)
+def start_clock_for_member(member):
+    users_sessions[member.name] = time.time()
 
-def seconds_to_minutes(seconds):
-    return round(seconds / 60)
+def weekly_hours(member):
+    minutes_sum = 0
+    with open("efficiency-bot/Efficiency-bot/time_logs.txt", "r") as time_logs:
+        for line in time_logs:
+            if line.split()[0] == member.name :
+                minutes_sum += float(line.split()[1])
+
+    return minutes_to_hours(minutes_sum)
 
 
-@bot.event
+@client.event
 async def on_ready():
-    print(f'We have logged in as {bot.user}')
+    print(f'We have logged in as {client.user}')
 
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
+@client.command()
+async def weekly_log(ctx):
+    await ctx.send(ctx.author + " Accumulated a Total of " + weekly_hours(ctx.author) + " Hours this Week.")
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Im watching...')
-
-@bot.event
+@client.event
 async def on_voice_state_update(member, before, after):
     print("Voice State Change")
     if before.channel == None and after.channel != None:
@@ -59,9 +63,7 @@ async def on_voice_state_update(member, before, after):
         member_joined_office(member)
     if before.channel != None and after.channel == None:
         if before.channel.name == 'Office': 
-           member_left_office(member)
+           await member_left_office(member)
        
 
-
-
-bot.run(TOKEN)
+client.run(TOKEN)
---
+++ utils.py
@@ -0,0 +1,13 @@
+import time
+
+
+
+def calculate_duration(member, start_time):
+    start_time = start_time
+    duration = time.time() - start_time
+    return seconds_to_minutes(duration)
+
+def seconds_to_minutes(seconds):
+    return round(seconds / 60)
+def minutes_to_hours(seconds):
+    return round(minutes_to_hours / 60, 3)
